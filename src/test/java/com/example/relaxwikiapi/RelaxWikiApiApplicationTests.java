3b9a3cd6b73a7f397220dbb0c77b8839ee0c40d5
ShaleenThilanka <shaleen.diss@gmail.com>
2023-02-02 10:31:09 +0530

    init

---
+++ src/test/java/com/example/relaxwikiapi/RelaxWikiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.relaxwikiapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelaxWikiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
